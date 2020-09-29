cd363e2c33cd39df88d8bc7987299600bbc7790f
Yao JC <510247045@qq.com>
2020-09-29 16:15:50 +0800

    文件打包目录设置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 module.exports = {
+  outputDir: path.join(__dirname, 'dist', process.env.VUE_APP_PROJECTNAME),
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = process.env.VUE_APP_MODULENAME
