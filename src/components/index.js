d42f3f9882132431f87da22a6f9c3924a956149a
joker <510247045@qq.com>
2020-11-04 21:38:48 +0800

    other:无

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 let files = require.context('.', false, /\.vue$/)
 files.keys().forEach((key) => {
-  if (key.match(/index\.js/)) {
+  if (key.match(/index\.js/)||!files(key).default.name) {
     return
   }
   Vue.component(files(key).default.name, files(key).default)
